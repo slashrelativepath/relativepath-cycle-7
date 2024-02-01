bc5de1d9e258ef29dfd9839481d42265b882e2f5
Richard Dixon <rvictordixon@gmail.com>
2024-02-01 18:50:45 -0500

    Add Windows local configuration.

---
+++ README.md
@@ -0,0 +1,5 @@
+#  relativepath cycle7
+
+On Windows:
+
+`Set-ExecutionPolicy Bypass -Scope Process -Force; .\local-windows.ps1`
---
+++ local-windows.ps1
@@ -0,0 +1,52 @@
+# install choco
+if ( get-command choco )
+{
+  write-host "choco already installed."
+}
+else
+{
+  write-host "Installing choco."
+  Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+}
+
+# install git
+if ( get-command git )
+{
+  write-host "git already installed."
+}
+else
+{
+  write-host "Installing git."
+  choco install -y git.install --params "'/GitAndUnixToolsOnPath /WindowsTerminal /NoAutoCrlf'" --force
+}
+
+# install nano
+if ( get-command nano )
+{
+  write-host "nano already installed."
+}
+else
+{
+  write-host "Installing nano."
+  choco install -y nano
+}
+
+if ( git config --global --get user.email )
+{
+  write-host "git user email already set."
+}
+else
+{
+  write-host "git user.email not set."
+  write-host "run: git config --global user.email YOUREMAIL"
+  write-host "run: git config --global user.name YOURNAME"
+}
+
+
+
+
+
+
+
+
+
