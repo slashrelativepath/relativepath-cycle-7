94e7994849522d65bd6df026e0cfef9828527b55
Alberto Castro <jaycastro100@gmail.com>
2024-02-07 18:29:51 -0500

    Add Darwin script

---
+++ local-darwin.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/zsh
+
+if (which brew)
+then
+  echo "brew already installed"
+else
+  echo "installing brew"
+  /bin/bash -y -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi  
+
+if (which nano)
+then
+  echo "nano already installed"
+else
+  echo "installing nano"
+  brew install -y nano
+fi
+
+if (which git)
+then
+  echo "git already installed"
+else
+  echo "installing git"
+  brew install -y git
+fi
+
