748e17abd4929fdc50a86337207dc7f9fb61cebc
David W <dswilliams1222@gmail.com>
2024-02-26 17:34:10 -0600

    Add check for VM creation.

--- create-vm.sh
+++ create-vm.sh
@@ -1,2 +1,7 @@
-multipass launch --name relativepath
-
+if (multipass info relativepath)
+then
+  echo "Relative Path VM already exists!"
+else
+  echo "Launching Relative Path VMs"
+  multipass launch --name relativepath
+fi
