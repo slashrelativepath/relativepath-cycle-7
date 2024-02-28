b5a72ad3f821bf6f478aa88cfb21680fe42a3f55
David W <dswilliams1222@gmail.com>
2024-02-28 17:40:27 -0600

    Remove SSH Key pairs after the VM is removed.

--- delete-vm.sh
+++ delete-vm.sh
@@ -1,1 +1,9 @@
 multipass delete relativepath --purge
+
+if (stat id_ed25519)
+then
+  echo "Removing SSH key pairs"
+  rm -f id_ed25519*
+  echo "SSH keypairs removed."
+else
+  echo "No SSH key detected."
