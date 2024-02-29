2f5a622f3f07f87a3524083b94b3a83fa2c01047
David W <dswilliams1222@gmail.com>
2024-02-28 18:22:03 -0600

    Remove SSH Key pairs after the VM is removed. Added YAML file deletion.

--- delete-vm.sh
+++ delete-vm.sh
@@ -1,11 +1,24 @@
 multipass delete relativepath --purge
-
 if (stat id_ed25519)
 then
+  echo "SSH Key already exist"
+else
+  echo "Creating SSH Key pair"
+  ssh-keygen -f "./id_ed25519" -t ed25519 -N ''
+if (stat id_ed25519)
   echo "Removing SSH key pairs"
   rm -f id_ed25519*
-  echo "SSH keypairs removed."
-  rm cloud-init.yaml
-  echo "Cloud YAML removed."
+  echo "SSH key pairs removed."
 else
   echo "No SSH key detected."
+fi
+
+if (stat cloud-init.yaml)
+  echo "Removing cloud-init.yaml"
+  rm cloud-init.yaml
+  echo "Cloud-init.yaml removed."
+else
+  echo "No cloud-init.yaml file detected."
+fi
+
+
