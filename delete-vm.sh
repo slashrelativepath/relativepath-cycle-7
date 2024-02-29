cf5bbf0f26c9be6b6b1262ecfd7926ad4845de9b
David W <dswilliams1222@gmail.com>
2024-02-28 18:27:35 -0600

    Remove SSH Key pairs after the VM is removed. Added YAML file deletion.

--- delete-vm.sh
+++ delete-vm.sh
@@ -1,11 +1,5 @@
 multipass delete relativepath --purge
 if (stat id_ed25519)
-then
-  echo "SSH Key already exist"
-else
-  echo "Creating SSH Key pair"
-  ssh-keygen -f "./id_ed25519" -t ed25519 -N ''
-if (stat id_ed25519)
   echo "Removing SSH key pairs"
   rm -f id_ed25519*
   echo "SSH key pairs removed."
