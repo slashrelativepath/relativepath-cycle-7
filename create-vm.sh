6cb2179ab3df6a0a05a4cf06d1bacc6ca7b3e79d
David W <dswilliams1222@gmail.com>
2024-02-29 18:03:50 -0600

    Automated SSH and CLoud init.

--- create-vm.sh
+++ create-vm.sh
@@ -6,10 +6,29 @@ else
   ssh-keygen -f "./id_ed25519" -t ed25519 -N ''
 fi
 
+if [ -f cloud-init.yaml ]
+then
+  echo -e "\n====Cloud-init.yaml present ===\n"
+else
+  echo -e "\n====Creating cloud-init.yaml====\n"
+  cat<<-EOF>cloud-init.yaml
+#cloud-config
+users:
+  - default
+  - name: $USER
+    sudo: ALL=(ALL) NOPASSWD:ALL
+    shell: /bin/bash
+    ssh_authorized_keys:
+      - $(cat id_ed25519.pub)
+EOF
+fi
+
 if (multipass info relativepath)
 then
   echo "Relative Path VM already exists!"
 else
   echo "Launching Relative Path VMs"
-  multipass launch --name relativepath
+  multipass launch --name relativepath --cloud-init cloud-init.yaml
 fi
+
+ssh -o StrictHostKeyChecking=no -i "./id_ed25519" $USER@$(multipass info relativepath | grep IPv4 | awk '{ print $2 }')
